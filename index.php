9f7ba032e90ee3864a8dc3dd5bf188586c32b9e7
jiangnan <jiangnan@me.com>
2015-07-05 16:01:00 +0800

    介绍（中文）

--- index.php
+++ index.php
@@ -1,92 +1,96 @@
 <?php
-	$css = "/css/home/index.css";
-	include("include/head.php");
+$css = "/css/home/index.css";
+include("include/head.php");
 ?>
 
 
 <!-- InstanceBeginEditable name="pan" -->
-	<ul id="pan">
-	<li></li>
-	</ul>
+<ul id="pan">
+    <li></li>
+</ul>
 <!-- InstanceEndEditable -->
 <script type="text/javascript" src="/js/gundong.js"></script>
 <div id="contents1">
-<!-- InstanceBeginEditable name="contents" -->
-	<div class="infos">
-		<div class="icons">
-		</div>
-		<div class="infoBox">
-			<div class="info">
-				<img src="/image/home/index_img_jieshao.jpg" />
-			</div>
-		
-			<div class="info">
-				<img src="/image/home/index_img_chanpin.jpg" />
-			</div>
-			
-			<div class="info">
-				<img src="/image/home/index_img_linian.jpg" />
-			</div>
-		</div>
-	</div>
+    <!-- InstanceBeginEditable name="contents" -->
+    <div class="infos">
+        <div class="icons">
+        </div>
+        <div class="infoBox">
+            <div class="info">
+                <img src="/image/home/index_img_jieshao.jpg"/>
+            </div>
 
-		<br>
+            <div class="info">
+                <img src="/image/home/index_img_chanpin.jpg"/>
+            </div>
 
-	<div class="boxLeft boxFull">
-		<div class="homeTitle">公司资质</div>
-		
-		<img alt="" src="/image/home/zizhi_e.jpg">
-		<img alt="" src="/image/home/zizhi_c.jpg">
-	</div>
+            <div class="info">
+                <img src="/image/home/index_img_linian.jpg"/>
+            </div>
+        </div>
+    </div>
 
-		<br>
+    <br>
 
-	<div class="boxLeft">
-		<div class="homeTitle">公司新闻</div>
-		<ul>
-			<li class="s12a"><img src="image/home/a2.gif" alt="new">2011年10月，佳与阳与日本栉田工业（株）就来年针对半导体，液晶等制造装置用特殊树脂空气压阀门的开发研制达成共识。</li>
-			<li class="s12a"><img src="image/home/a2.gif" alt="new">2011年9月6日至7日，佳与阳携手合作伙伴日本栉田工业（株）精彩亮相“中日制造采购洽谈会－－上海2011展会”。在展会上，佳与阳的工程塑料切削加工产品－－药液、纯水供给用树脂空气压阀门－－得到了极大的关注。</li>
-			<li class="s12a"><img src="image/home/a2.gif" alt="new">2011年7月30日，日本大和リテック（株）榊原社長考察佳与阳。佳与阳总经理孟伟斌陪同考察，双方还就工程塑料的切削加工工业前景、高精密机械加工、公司合作等等事项进行了深入的交流。</li>
-			<li class="s12a"><img src="image/home/a2.gif" alt="new">2011年3月，佳与阳成立。日本栉田工业（株）及三菱重工空调系统（上海）有限公司等领导光临道贺。支持公司成立！新闻更新</li>
-		</ul>
-	</div>
-	<div class="boxRight">
-<!-- 		<p class="btn"> -->
-<!-- 			<br><br><br> -->
-<!-- 			中国区<br>TEL 86-510-85606316<br>FAX　86-510-85626316<br>MOBILE　86-13771153090<br><br> -->
-<!-- 			日本区<br>MOBILE　0081-9010959278 -->
-			
-			<table class="btn" height=180px>
-				<tr>
-					<td height="35px">&nbsp;</td>
-				</tr>
-				<tr>
-					<td colspan＝2>中国区</td>
-				</tr>
-				<tr>
-					<td>TEL</td>
-					<td>86-510-85606316</td>
-				</tr>
-				<tr>
-					<td>FAX</td>
-					<td>86-510-85626316</td>
-				</tr>
-				<tr>
-					<td>MOBILE</td>
-					<td>86-13771153090</td>
-				</tr>
-			
-				<tr>
-					<td colspan＝2>日本区</td>
-				</tr>
-				<tr>
-					<td>MOBILE</td>
-					<td>0081-9010959278</td>
-				</tr>
-			</table>
-<!-- 		</p> -->
-	</div>
+    <div class="boxLeft boxFull">
+        <div class="homeTitle">公司资质</div>
 
-<?php
-	include("include/footer.php");
-?>
+        <img alt="" src="/image/home/zizhi_e.jpg">
+        <img alt="" src="/image/home/zizhi_c.jpg">
+    </div>
+
+    <br>
+
+    <div class="boxLeft">
+        <div class="homeTitle">公司新闻</div>
+        <ul>
+            <li class="s12a"><img src="image/home/a2.gif" alt="new">2011年10月，佳与阳与日本栉田工业（株）就来年针对半导体，液晶等制造装置用特殊树脂空气压阀门的开发研制达成共识。
+            </li>
+            <li class="s12a"><img src="image/home/a2.gif" alt="new">2011年9月6日至7日，佳与阳携手合作伙伴日本栉田工业（株）精彩亮相“中日制造采购洽谈会－－上海2011展会”。在展会上，佳与阳的工程塑料切削加工产品－－药液、纯水供给用树脂空气压阀门－－得到了极大的关注。
+            </li>
+            <li class="s12a"><img src="image/home/a2.gif" alt="new">2011年7月30日，日本大和リテック（株）榊原社長考察佳与阳。佳与阳总经理孟伟斌陪同考察，双方还就工程塑料的切削加工工业前景、高精密机械加工、公司合作等等事项进行了深入的交流。
+            </li>
+            <li class="s12a"><img src="image/home/a2.gif" alt="new">2011年3月，佳与阳成立。日本栉田工业（株）及三菱重工空调系统（上海）有限公司等领导光临道贺。支持公司成立！新闻更新
+            </li>
+        </ul>
+    </div>
+    <div class="boxRight">
+        <!-- 		<p class="btn"> -->
+        <!-- 			<br><br><br> -->
+        <!-- 			中国区<br>TEL 86-510-85606316<br>FAX　86-510-85626316<br>MOBILE　86-13771153090<br><br> -->
+        <!-- 			日本区<br>MOBILE　0081-9010959278 -->
+
+        <table class="btn" height=180px>
+            <tr>
+                <td height="35px">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan＝2>中国区</td>
+            </tr>
+            <tr>
+                <td>TEL</td>
+                <td>86-510-85606316</td>
+            </tr>
+            <tr>
+                <td>FAX</td>
+                <td>86-510-85626316</td>
+            </tr>
+            <tr>
+                <td>MOBILE</td>
+                <td>86-13771153090</td>
+            </tr>
+
+            <tr>
+                <td colspan＝2>日本区</td>
+            </tr>
+            <tr>
+                <td>MOBILE</td>
+                <td>0081-9010959278</td>
+            </tr>
+        </table>
+        <!-- 		</p> -->
+    </div>
+
+    <?php
+    include("include/footer.php");
+    ?>
--- jieshao/index.php
+++ jieshao/index.php
@@ -1,60 +1,92 @@
 <?php
-	$css = "/css/jieshao/index.css";
-	include("../include/head.php");
+$css = "/css/jieshao/index.css";
+include("../include/head.php");
 ?>
 
-<!-- InstanceBeginEditable name="pan" -->
-	<ul id="pan">
-	<li><a href="/index.php">首页</a></li>
-	<li>&nbsp;&gt;&nbsp;公司介绍</li>
-	</ul>
-<!-- InstanceEndEditable -->
+    <!-- InstanceBeginEditable name="pan" -->
+    <ul id="pan">
+        <li><a href="/index.php">首页</a></li>
+        <li>&nbsp;&gt;&nbsp;公司介绍</li>
+    </ul>
+    <!-- InstanceEndEditable -->
 
-<div id="contents1">
-<!-- InstanceBeginEditable name="contents" -->
+    <div id="contents1">
+        <!-- InstanceBeginEditable name="contents" -->
 
-	<h2>会社案内</h2>
-<div id="main">
-    <h3>关于佳与阳</h3>
-		<div class="box">
-			
-			<p class="s13a">
-			无锡佳与阳精密机械有限公司是国内少有的以专业工程塑料切削加工为主的企业。本公司与日本知名企业技术合作，生产汽车、电子电器设备、医疗设备、通信设备等工程塑料部品的切削加工，及半导体、液晶等制造装置的药液、纯水供给用树脂空气压阀门的设计加工。
-		  	<br><br>
-		  </p>
-		  <p class="s13a">
-		  本公司融合日本的先端技术和管理方式，引进原装进口数控设备和检测设备，在与日本同等的加工恒温环境下，保证产品质量，从单件试样到批量生产及纳期上都能满足客户要求。
-		  <br><br>
-		  </p>
-		  <p class="s13a">
-		  本公司主要使用素材有POM聚甲醛；PP聚丙烯；PVC聚氯乙烯；UPE超高分子聚乙烯；PC聚碳酸酯；PTFE聚四氟乙烯；PEEK聚醚醚酮；PPS聚苯硫醚，MC尼龙等(国内和进口材料齐全)。
-			<br><br>
-			</p>
-			<p class="s13a">	
-			*按客户要求，也承接铜材和铝材为主的配件产品。
-		  </p>
-		  <br>
-		<img alt="" src="/image/jieshao/guanyu1.jpg">
-		<img alt="" src="/image/jieshao/shebei4.jpg">
-		
-		</div>
-		
+        <h2>会社案内</h2>
 
-        <h3>合作企业</h3>
-		<div class="box">
-			<div class="left">
-				<p class="s13a padding">
-				佳与阳与日本<a href="http://www.kushida-industry.co.jp" target="_blank">櫛田工業(株)工程塑料专业公司</a>技术合作。
-			   </p>
-			 </div>
-		  	<p class="right">	
-		  		<img alt="" src="/image/jieshao/qiye.jpg">
-		  	</p>
-		</div>
-</div>
+        <div id="main">
+            <h3>关于佳与阳</h3>
+
+            <div class="box">
+
+                <p class="s13a">
+                    无锡佳与阳精密机械有限公司是国内少有的以专业工程塑料切削加工为主的企业。本公司与日本知名企业技术合作，生产汽车、电子电器设备、医疗设备、通信设备等工程塑料部品的切削加工，及半导体、液晶等制造装置的药液、纯水供给用树脂空气压阀门的设计加工。
+                    <br><br>
+                </p>
+
+                <p class="s13a">
+                    本公司融合日本的先端技术和管理方式，引进原装进口数控设备和检测设备，在与日本同等的加工恒温环境下，保证产品质量，从单件试样到批量生产及纳期上都能满足客户要求。
+                    <br><br>
+                </p>
+
+                <p class="s13a">
+                    本公司主要使用素材有POM聚甲醛；PP聚丙烯；PVC聚氯乙烯；UPE超高分子聚乙烯；PC聚碳酸酯；PTFE聚四氟乙烯；PEEK聚醚醚酮；PPS聚苯硫醚，MC尼龙等(国内和进口材料齐全)。
+                    <br><br>
+                </p>
+
+                <p class="s13a">
+                    *按客户要求，也承接铜材和铝材为主的配件产品。
+                </p>
+                <br>
+            </div>
+            <div class="jieshao">
+                <div class="jieshaoLeft">
+                    <img alt="" src="/image/jieshao/guanyu1.jpg">
+                </div>
+                <div class="jieshaoRight">
+                    <div class="homeTitle">服务领域</div>
+                    <ul>
+                        <li class="s12a">半导体装置用工程塑料零件加工</li>
+						<li class="s12a">电子电器工程塑料零件加工</li>
+						<li class="s12a">医疗设备器材用工程塑料零件加工</li>
+						<li class="s12a">通信设备器材用工程零件加工</li>
+						<li class="s12a">其他种类试制品加工</li>
+                    </ul>
+                </div>
+            </div>
+            
+            <br>
+            	
+            <div class="jieshao">
+                <div class="jieshaoLeft">
+                    <img alt="" src="/image/jieshao/guanli1.jpg">
+                </div>
+                <div class="jieshaoRight">
+                    <div class="homeTitle">企业模式</div>
+                    <ul>
+                        <li class="s12a">融合日本的先端技术和管理方式</li>
+						<li class="s12a">引进原装进口数控设备和检测设备，保证产品质量</li>
+						<li class="s12a">和客户建立良好的信誉，为客户提供最优质的产品</li>
+                    </ul>
+                </div>
+            </div>
+            <h3>合作企业</h3>
+
+            <div class="box">
+                <div class="left">
+                    <p class="s13a padding">
+                        佳与阳与日本<a href="http://www.kushida-industry.co.jp" target="_blank">櫛田工業(株)工程塑料专业公司</a>技术合作。
+                    </p>
+                </div>
+                <p class="right">
+                    <img alt="" src="/image/jieshao/qiye.jpg">
+                </p>
+            </div>
+        </div>
 
 
-<!-- InstanceEndEditable -->
+        <!-- InstanceEndEditable -->
 	<?php
 	include("../include/footer.php");
 ?>
