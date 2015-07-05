afe68bd816e0b72cc4d5f170a64b12b7444a6fa6
changhai <changhai@baidu.com>
2015-07-05 12:39:58 +0800

    fixed bug

--- js/gundong.js
+++ js/gundong.js
@@ -1,40 +1,40 @@
 var current = 0;
 var count;
-var top;
 $(function() {
-	var interval = 5000;
-	count = $(".ad").size();
-	top = $(".adBox").position().top;
-	$(".ad").each(function(i) {
-		var icon = $("<div class='icon'>" + (i + 1) + "</div>");
-		var ad = $(this);
-		icon.click(function() {
-			nextTo(i);
-		});
-		$(".ads .icons").append(icon); 
-	});
-	$(".ad:first").clone().appendTo($(".adBox"));
-	var si = setInterval("next()", interval);
-	$(".ads").hover(function() {
-		clearInterval(si);
-	}, function() {
-		si = setInterval("next()", interval);
-	});
-	next();
+    var interval = 2000;
+    count = $(".ad").size();
+    $(".ad").each(function(i) {
+        var icon = $("<div class='icon'>" + (i + 1) + "</div>");
+        var ad = $(this);
+        icon.click(function() {
+            nextTo(i);
+        });
+        $(".ads .icons").append(icon);
+    });
+    $(".ad:first").clone().appendTo($(".adBox"));
+    var si = setInterval("next()", interval);
+    $(".ads").hover(function() {
+        clearInterval(si);
+    }, function() {
+        si = setInterval("next()", interval);
+    });
+    next();
 });
 function next() {
-	if(current > count)
-		current = 1;
-	nextTo(current++);
+    if(current > count)
+        current = 1;
+    nextTo(current++);
 }
 function nextTo(i) {
-	$(".ads .icons .icon").removeClass("on");
-	$(".ads .icons .icon").eq(i >= count ? 0 : i).addClass("on");
-	if($(".adBox").position().top + $(".adBox").height() <= 350) {
-		$(".adBox").offset({top: 0});
-		$(".adBox").offset({top: -$(".adBox").position().top});
-	}
-	$(".adBox").animate({
-		top: top - 350 * i
-	}, 1000);
+    $(".ads .icons .icon").removeClass("on");
+    $(".ads .icons .icon").eq(i >= count ? 0 : i).addClass("on");
+    if($(".adBox").position().top + $(".adBox").height() <= 350) {
+        $(".adBox").offset({top: 0});
+        $(".adBox").offset({top: -$(".adBox").position().top});
+    }
+    if(i > 0) {
+        $(".adBox").animate({
+            top: "-=350"
+        }, 1000);
+    }
 }
