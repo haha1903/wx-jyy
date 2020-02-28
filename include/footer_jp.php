a2e86fa67277b0a8c10c968b3408b58c78d74934
haha1903 <haha1903@gmail.com>
2020-02-28 18:28:16 +0800

    beian

--- include/footer_jp.php
+++ include/footer_jp.php
@@ -4,6 +4,7 @@
 
 <div id="footer">
 	<p class="s12a copy">Copyright(c)&nbsp;2011&nbsp;jia&yang&nbsp;industry,Inc.<br>All&nbsp;Rights&nbsp;Reserved.</p>
+	<p class="s12a copy"><a class="beian" href="http://beian.miit.gov.cn/" target="_blank">苏ICP备11071919号-1</a></p>
 </div>
 <div id="gov">
 	<script id="jsgovicon" src="http://odr.jsdsgsxt.gov.cn:8081/mbm/app/main/electronic/js/govicon.js?siteId=699e3d55d2ac4cfb94d94ce1bcbc8dd4&width=100&height=137&type=1" type="text/javascript" charset="utf-8"></script>
