e8ed7967ff68d0a6824cda91373d732423cbbc0c
Hai Chang <haichang@microsoft.com>
2026-05-16 17:15:07 +1000

    switch to WordPress on AKS, drop legacy PHP site

--- README.md
+++
@@ -1,4 +0,0 @@
-# Release Log
-
-1. 9.11 update image
-1. 12.18 update image
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-docker build --platform linux/amd64 . -t ghcr.io/haha1903/wx-jyy:latest --no-cache --pull
-docker push ghcr.io/haha1903/wx-jyy:latest
