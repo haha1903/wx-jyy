bb6ff347366c2f2572d2a021efe7a1183e6aa40a
Hai Chang <haichang@microsoft.com>
2026-05-16 16:51:32 +1000

    ci: deploy to haiaks via github actions + ghcr + oidc

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-docker build . -t haiacr.azurecr.io/wx-jyy --no-cache --pull
-docker push haiacr.azurecr.io/wx-jyy
+#!/bin/bash
+set -e
+docker build --platform linux/amd64 . -t ghcr.io/haha1903/wx-jyy:latest --no-cache --pull
+docker push ghcr.io/haha1903/wx-jyy:latest
